4185800f652a02710f2eb496c872caa4874c68f0
Charlie Poole <charliepoole@gmail.com>
2017-06-03 18:02:59 -0700

    Add demo-assembly for use in manual testing of the gui

---
+++ src/demo-assembly/GenericTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NUnitTestDemo
+{
+    [TestFixture(typeof(int))]
+    public class GenericTests<T>
+    {
+        [Test, ExpectPass]
+        public void TestIt() { }
+    }
+
+    [ExpectPass]
+    [TestFixture(typeof(ArrayList))]
+    [TestFixture(typeof(List<int>))]
+    public class GenericTests_IList<TList> where TList : IList, new()
+    {
+        private IList list;
+
+        [SetUp]
+        public void CreateList()
+        {
+            this.list = new TList();
+        }
+
+        [Test]
+        public void CanAddToList()
+        {
+            list.Add(1); list.Add(2); list.Add(3);
+            Assert.AreEqual(3, list.Count);
+        }
+    }
+}
---
+++ src/demo-assembly/Theories.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NUnitTestDemo
+{
+    public class Theories
+    {
+        [Datapoints]
+        int[] data = new int[] { 0, 1, 42 };
+
+        [Theory, ExpectPass]
+        public void Theory_AllCasesSucceed(int a, int b)
+        {
+            Assert.That(a + b, Is.EqualTo(b + a));
+        }
+
+        [Theory, ExpectMixed]
+        public void Theory_SomeCasesAreInconclusive(int a, int b)
+        {
+            Assume.That(b != 0);
+        }
+
+        [Theory, ExpectMixed]
+        public void Theory_SomeCasesFail(int a, int b)
+        {
+            Assert.That(b != 0);
+        }
+    }
+}
